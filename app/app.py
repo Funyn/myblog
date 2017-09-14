3d1199f6957d40dd95212f34b3d3c99fb4734889
Funyn <31060879+Funyn@users.noreply.github.com>
2017-09-14 19:44:01 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-09-14 19:42:50
+# @Author  : Funyn (461547449@qq.com)
+# @Link    : none
+# @Version : $0.0.1$
+
 from flask import Flask
 
 app = Flask(__name__)
