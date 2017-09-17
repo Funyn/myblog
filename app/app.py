7a132628fad47c98beaab314179a90119100e69c
Funyn <31060879+Funyn@users.noreply.github.com>
2017-09-17 14:45:15 +0800

    添加flask_sqlalchemy对象

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Date    : 2017-09-14 19:42:50
+# @Date    : 2017-09-17 14:42:50
 # @Author  : Funyn (461547449@qq.com)
 # @Link    : none
 # @Version : $0.0.1$
 
 from flask import Flask
+from exts import db
+import config
 
 app = Flask(__name__)
+app.config.from_object(config)
+db.init_app(app)
 
 from views import *
 
